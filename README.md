ec8ce06c8a62b776e0a9b80d2a10fbf12cd73aa2
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-06-06 14:37:52 +0530

    I Love You ❤

---
+++ README.md
@@ -0,0 +1,20 @@
+## Multipurpose Bot 
+
+An Multi purpose Bot Which Can :-
+* ✅Rename Telegram Files 
+* ✅Convert Files into Video
+
+
+### You can tap the Deploy To Heroku button below to deploy straight to Heroku!
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/No-OnE-Kn0wS-Me/FileRenameBot)
+
+If you Find Any Bugs Or Want to Give Your Feedbacks Then Kindly Contact Me Through [Telegram ](https://telegram.dog/No_OnE_Kn0wS_Me) 
+Also Support Our Channel [Mai_bOTs](https://telegram.dog/Mai_bOTs) 
+
+## Credits, and Thanks to Beloved Developers ;
+
+* [SpEcHlDe](https://telegram.dog/SpEcHlDe) 
+* [Dan Tès](https://telegram.dog/haskell) 
+* [Yoily](https://telegram.dog/YoilyL)
+* [Anand](https://telegram.dog/Anandpskerala)
---
+++ translation.py
@@ -0,0 +1,33 @@
+class Translation(object):
+    START_TEXT = """ <b> Hi {} , Iam A Multifunctional Telegram Bot by @shamilnelli 😇, Functions as both Renamer and Converter Bot With Permanent Thumbnail support! 😎</b> \n
+
+<b>Bot By : MwK Projects </b> \n 
+"""
+
+    RENAME_403_ERR = "What Are You Doing? Bruuh!!"
+    BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @No_OnE_Kn0wS_Me For more Details.. " 
+    ABS_TEXT = "What Are You Trying To Do,Mate?"
+    UPGRADE_TEXT = "CONTACT @No_OnE_Kn0wS_Me "
+    DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
+    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our Channel @mai_bots</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "CONTACT @No_OnE_Kn0wS_Me "
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @No_OnE_Kn0wS_Me "
+    SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
+    REPLY_TO_FILE_FOR_CONVERT = "<b> Please Reply To An File With /c2v To Convert It Into Streamable video File</b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_THUMB_FOUND = "No Thumbnail found"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
+    ABOUT_ME = """<b>Me : \n A Telegram File Renamer com Converter. \n• Permanent Thumbnail Supported .\n \n Donate My Owner @shamilnelli If You Found Meh Useful</b>"""
+    HELP_USER = """Checkout The Available Commands Here \n\n Do Follow @mwklinks ❤️"""
+    RENAME_HELP = """Here are The Available Commands In Reanme \n\n\n • <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
+    C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n • <code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
+    THUMBNAIL_HELP = """Here Are The Available Commands In Custom Thumbnail \n\n\n •  Send A Photo To Set The Custom Thumbnail \n • <code>/showthumb</code> : For Checking The Current Thumbnail \n • <code>/delthumb</code> : For Deleting The Current Saved Thumbnail"""
