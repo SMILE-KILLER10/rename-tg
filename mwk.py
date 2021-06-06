ece8b45962d1596c78a8618a194cde83fcf914c7
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-06-06 15:10:11 +0530

    I Love You ❤
    A Multifunctional Telegram Bot by @shamilnelli 😇, Functions as both Renamer and Converter Bot With Permanent Thumbnail support! 😎

--- mwk.py
+++ mwk.py
@@ -6,8 +6,6 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 import os
 
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
     from config import Config
 
 import pyrogram
