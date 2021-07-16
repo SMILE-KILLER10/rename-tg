d68a1ae003421a31668d5dc5552e91c5732cce3b
SMILE-KILLER10 <85184491+SMILE-KILLER10@users.noreply.github.com>
2021-07-16 10:21:52 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -11,7 +11,7 @@ class Translation(object):
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our GROUP @NAZRIYAOFFTOPIC</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
-    NOT_AUTH_USER_TEXT = "CONTACT @shamilnelli"
+    NOT_AUTH_USER_TEXT = "CONTACT @smile_killer_010"
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @smile_killer_010 "
     SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
     DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
